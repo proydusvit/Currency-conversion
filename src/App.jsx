4b5dba675cb6fc19d52f1e4ce2050b16436abaf3
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2023-12-07 19:17:40 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './CurrencyConverter.module.scss';
 
+
 const CurrencyConverter = () => {
+
   const [fromCurrency, setFromCurrency] = useState('USD');
-  const [toCurrency, setToCurrency] = useState('EUR');
+  const [toCurrency, setToCurrency] = useState('UAH');
   const [amount, setAmount] = useState(1);
+
   const [exchangeRates, setExchangeRates] = useState({
     USD: 1,
     EUR: 1,
-    UAH: 1
+    UAH: 1,
+    ZLT:1
   });
+
   const [rateBuy, setRateBuy] = useState({
     USD: 1,
     EUR: 1,
-    UAH: 1
+    UAH: 1,
+ 
   });
+
   const [convertedAmount, setConvertedAmount] = useState();
 
   useEffect(() => {
+
     const fetchExchangeRates = async () => {
+
       try {
         const response = await fetch('https://api.monobank.ua/bank/currency');
         const data = await response.json();
+        const zltfilter = data.find(currency => currency.currencyCodeA === 985);
 
         setExchangeRates({
           USD: data[0].rateSell,
           EUR: data[1].rateSell,
-          UAH: 1
+          UAH: 1,
+          ZLT: zltfilter.rateCross,
         });
 
         setRateBuy({
           USD: data[0].rateBuy,
           EUR: data[1].rateBuy,
-          UAH: 1
+          UAH: 1,
+
         });
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -42,29 +55,42 @@ const CurrencyConverter = () => {
     fetchExchangeRates();
   }, []);
 
+
   useEffect(() => {
     const result = (amount / exchangeRates[fromCurrency]) * exchangeRates[toCurrency];
     setConvertedAmount(result.toFixed(2));
+
   }, [amount, fromCurrency, toCurrency, exchangeRates]);
+
 
   const handleFromCurrencyChange = e => {
     setFromCurrency(e.target.value);
   };
+
 
   const handleToCurrencyChange = e => {
     setToCurrency(e.target.value);
   };
 
   const handleAmountChange = e => {
-    setAmount(e.target.value);
+    const inputValue = e.target.value;
+
+    if (inputValue >= 0) {
+    setAmount(inputValue);
+    } 
+    else {
+      setAmount(0);
+    }
+  
   };
 
   
+
   return (
-    <div className={styles['currency-converter']}>
+    <div className={styles.currency}>
     <header className={styles.header}>
         <h1>Курс валют:</h1>
-        <div className={styles['currency-info']}>
+        <div className={styles.currency_info}>
           <div>
             <h2>Продаж</h2>
             <p>USD: {exchangeRates.USD}</p>
@@ -74,39 +100,45 @@ const CurrencyConverter = () => {
             <h2>Купівля</h2>
             <p>USD: {rateBuy.USD}</p>
             <p>EUR: {rateBuy.EUR}</p>
+            
           </div>
         </div>
       </header>
 
-      <h2>Currency Converter</h2>
-      <div className={styles['conversion-section']}>
-        <label>From Currency:</label>
+      <h2>Конвертер валют</h2>
+      <div className={styles.conversion_section}>
+        
+        <label>З валюти:</label>
         <select value={toCurrency} onChange={handleToCurrencyChange}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="UAH">UAH</option>
+            <option value="ZLT">ZLT</option>
         </select>
-        <br />
-        <label>To Currency:</label>
+     
+        <label>До валюти:</label>
         <select value={fromCurrency} onChange={handleFromCurrencyChange}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="UAH">UAH</option>
+            <option value="ZLT">ZLT</option>
         </select>
-        <br />
+
         <div>
-          <label>Amount:</label>
+          <label>Сума:</label>
           <input type="number" value={amount} onChange={handleAmountChange} />
-          <br />
+    
         </div>
-        <br />
+     
       </div>
-      <div className={styles.result}>
-        {convertedAmount && (
-          <p>
-            Converted Amount: {amount} {toCurrency} = {convertedAmount} {fromCurrency}
-          </p>
-        )}
+      <div className={styles.conversion_result}>
+      
+          <>
+            <p> У мене є: {amount} {toCurrency}  </p>
+            
+            <p>Я отримаю: {convertedAmount} {fromCurrency}</p>
+          </>
+       
       </div>
     </div>
   );
